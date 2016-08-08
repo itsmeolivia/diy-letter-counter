85a83af4578142d63eb47d6ed120e8bca3370579
Olivia Nordquist <nolivia@umich.edu>
2016-08-07 19:43:30 -0700

    MVP

--- main.js
+++ main.js
@@ -6,7 +6,35 @@ new Vue({
   },
   computed: {
     groups: function () {
-      return [{count: 3, letters: ["e", "f"]}, {count: 2, letters: ["a", "b"]}]
+      var dict = {}
+      for (var i = 0; i < this.message.length; i++) {
+        var char = this.message[i]
+        if (char in dict) {
+          dict[char] += 1
+        } else {
+          dict[char] = 1
+        }
+      }
+
+      var groups = {}
+      for (var letter in dict) {
+        if (!dict.hasOwnProperty(letter)) { continue }
+        var count = dict[letter]
+        if (count in groups) {
+          groups[count].push(letter)
+        } else {
+          groups[count] = [letter]
+        }
+      }
+
+      var arr = []
+      for (var count in groups) {
+        if (!groups.hasOwnProperty(count)) { continue }
+        var letters = groups[count]
+        arr.push({count: count, letters: letters})
+      }
+
+      return arr
     }
   }
 })
